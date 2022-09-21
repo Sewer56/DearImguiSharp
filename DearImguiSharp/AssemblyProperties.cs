5026df744289480c07b46237c8c3ff83eb0b60e5
Sewer 56 <seweratwork@gmail.com>
2022-09-21 02:40:36 +0100

    Added: SkipLocalsInit & .NET 5 Target. And More NuGet Props

---
+++ DearImguiSharp/AssemblyProperties.cs
@@ -0,0 +1,15 @@
+#if NET5_0_OR_GREATER
+using System.Runtime.CompilerServices;
+#endif
+
+#if NET5_0_OR_GREATER
+[module: SkipLocalsInit]
+#endif
+
+namespace DearImguiSharp
+{
+    public class AssemblyProperties
+    {
+        
+    }
+}
